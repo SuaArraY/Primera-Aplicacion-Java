8bc7d28220b72c6eb2b8a03ea36c54c09f923120
SuaArraY <1313178@senati.pe>
2023-03-09 21:13:37 -0500

    Boton

--- app/src/main/java/com/example/primeraplicacion/MainActivity.java
+++ app/src/main/java/com/example/primeraplicacion/MainActivity.java
@@ -1,14 +1,47 @@
 package com.example.primeraplicacion;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.Notification;
+import android.app.PendingIntent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.BufferedInputStream;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btNotificacion;
+    private PendingIntent pendingIntent;
+    private final static String CHANNEL_ID = "NOTIFICACION";
+    private final static int NOTIFIACION_ID = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btNotificacion = findViewById(R.id.btnNotificacion);
+        btNotificacion.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                createNotification();
+            }
+        });
+    }
+    private  void createNotification(){
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext(),CHANNEL_ID);
+        builder.setSmallIcon(R.drawable.ic_sms_black_24dp);
+        builder.setContentTitle("Notificacion Android");
+        builder.setContentText("Bienvenido Yossec");
+        builder.setColor(color.BLUE);
+        builder.setPriority(NotificationCompat.PRIORITY_DEFAULT);
+        builder.setVibrate(new long[]{1000,1000,1000,1000,10000});
+        builder.setDefaults(Notification.DEFAULT_SOUND);
+
+        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(getApplicationContext());
+        notificationManagerCompat.notify(NOTIFIACION_ID);
     }
 }
